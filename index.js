e9ecab1027d180fefa43ebe88bc08772d66befba
Ard-Jan van Etten <ardjanve@gmail.com>
2023-12-18 10:34:39 +0100

    e

--- index.js
+++ index.js
@@ -27,23 +27,34 @@ client.on("messageCreate", async msg => {
     if (msg.author.bot) return;
     const route = msg.content;
     planReis(multiReis(route)).then(async (reis) => {
-        const messages = formatteerReis(reis).split("\n").reduce((resultaatregels, line) => {
-            const lengte = resultaatregels.at(-1).reduce((a, b) => a.length + b.length + 1, 0);
-            if (lengte + line.length >= 1990) {
-                return [...resultaatregels, [line]];
-            } else {
-                return [...resultaatregels.slice(0, -1), [...resultaatregels.at(-1), line]];
+        // const messages = formatteerReis(reis).split("\n").reduce((resultaatregels, line) => {
+        //     const lengte = resultaatregels.at(-1).reduce((a, b) => a.length + b.length + 1, 0);
+        //     if (lengte + line.length >= 1990) {
+        //         return [...resultaatregels, [line]];
+        //     } else {
+        //         return [...resultaatregels.slice(0, -1), [...resultaatregels.at(-1), line]];
+        //     }
+        // }, [[]])
+        // .map((regels) => regels.join("\n"));
+
+        // for (const message of messages) {
+        //     await msg.channel.send("```" + message + "```");
+        // }
+
+        const response = formatteerReis(reis);
+        if (response.length < 1990) {
+            await msg.channel.send("```" + response + "```");
+        } else {
+            await msg.channel.send({
+                files: [{
+                    attachment: Buffer.from(response, "utf-8"),
+                    name: 'trein.txt'
+                  }]
+                });
             }
-        })
-        .map((regels) => regels.join("\n"));
-
-        for (const message of messages) {
-            await msg.channel.send("```" + message + "```");
-        }
-
         
-    // }).catch(console.error);
-    }).catch((_) => msg.react("😕"));
+    }).catch(console.error);
+    // }).catch((_) => msg.react("😕"));
 });
 
 client.login(process.env.DISCORD_API);
